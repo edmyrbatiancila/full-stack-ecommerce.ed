b1c1ac4fb0685f0f479670b8d5b705eea8262e07
Edmyr <127021800+edmyrbatiancila@users.noreply.github.com>
2024-06-01 01:20:25 +0800

    updated code and added files/folders

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -11,14 +11,37 @@ const LoginPopup = ({setShowLogin}) => {
       <form className="login-popup-container" action="">
         <div className="login-popup-title">
           <h2>{currentState}</h2>
-          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
         </div>
         <div className="login-popup-inputs">
-            <input type="text" placeholder='Your Name' required />
-            <input type="email" placeholder='Your Email' required />
-            <input type="password" placeholder='Password' required />
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your Name" required />
+          )}
+          <input type="email" placeholder="Your Email" required />
+          <input type="password" placeholder="Password" required />
         </div>
-        <button>{currentState === "Sign Up" ? "Create Account" : "Login"}</button>
+        <button>
+          {currentState === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+        {currentState === "Login" ? (
+          <p>
+            Create a new account? <span onClick={() => setCurrentState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account? <span onClick={() => setCurrentState("Login")}>Login here</span>
+          </p>
+        )}
       </form>
     </div>
   );
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -8,48 +8,52 @@ const Navbar = ({ setShowLogin }) => {
     const [menu, setMenu] = useState("home");
 
     return (
-    <div className="navbar">
-      <img src={assets.logo} alt="logo of the website" className="logo" />
-      <ul className="navbar-menu">
-        <Link
-          to="/"
-          onClick={() => setMenu("home")}
-          className={menu === "home" ? "active" : ""}
-        >
-          home
+      <div className="navbar">
+        <Link to="/">
+          <img src={assets.logo} alt="logo of the website" className="logo" />
         </Link>
-        <a
-          href="#explore-menu"
-          onClick={() => setMenu("menu")}
-          className={menu === "menu" ? "active" : ""}
-        >
-          menu
-        </a>
-        <a
-          href="#app-download"
-          onClick={() => setMenu("mobile-app")}
-          className={menu === "mobile-app" ? "active" : ""}
-        >
-          mobile-app
-        </a>
-        <a
-          href="#footer"
-          onClick={() => setMenu("contact-us")}
-          className={menu === "contact-us" ? "active" : ""}
-        >
-          contact us
-        </a>
-      </ul>
-      <div className="navbar-right">
-        <img src={assets.search_icon} alt="search icon" />
-        <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+        <ul className="navbar-menu">
+          <Link
+            to="/"
+            onClick={() => setMenu("home")}
+            className={menu === "home" ? "active" : ""}
+          >
+            home
+          </Link>
+          <a
+            href="#explore-menu"
+            onClick={() => setMenu("menu")}
+            className={menu === "menu" ? "active" : ""}
+          >
+            menu
+          </a>
+          <a
+            href="#app-download"
+            onClick={() => setMenu("mobile-app")}
+            className={menu === "mobile-app" ? "active" : ""}
+          >
+            mobile-app
+          </a>
+          <a
+            href="#footer"
+            onClick={() => setMenu("contact-us")}
+            className={menu === "contact-us" ? "active" : ""}
+          >
+            contact us
+          </a>
+        </ul>
+        <div className="navbar-right">
+          <img src={assets.search_icon} alt="search icon" />
+          <div className="navbar-search-icon">
+            <Link to="/Cart">
+              <img src={assets.basket_icon} alt="" />
+            </Link>
+            <div className="dot"></div>
+          </div>
+          <button onClick={() => setShowLogin(true)}>sign in</button>
         </div>
-        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
