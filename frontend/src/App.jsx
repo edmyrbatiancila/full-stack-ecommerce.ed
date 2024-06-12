e97667c2f7bce9095feeb0efd31bf3e161832b2f
Edmyr <127021800+edmyrbatiancila@users.noreply.github.com>
2024-06-13 00:58:49 +0800

    update code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Navbar from './components/navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
