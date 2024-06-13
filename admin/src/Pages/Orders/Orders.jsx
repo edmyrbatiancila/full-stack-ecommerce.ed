5cc6b55a4d21ab094894cf0acc39063101d33fbd
Edmyr <127021800+edmyrbatiancila@users.noreply.github.com>
2024-06-13 21:59:35 +0800

    update codes

--- admin/src/Pages/Orders/Orders.jsx
+++ admin/src/Pages/Orders/Orders.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
 import './Orders.css';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-const Orders = () => {
+const Orders = ({url}) => {
+
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  }
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
   return (
     <div>
       orders
